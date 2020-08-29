0c8732b0f1792e10b88ee5d20bd82cef072707f0
Jacobo-Miguel <65904635+Jacobo-Miguel@users.noreply.github.com>
2020-08-29 11:24:06 -0300

    Update main.c

--- codigo/src/main.c
+++ codigo/src/main.c
@@ -3,97 +3,21 @@
 #include <string.h>
 #include "lib.h"
 
-
 int main(){
 	datos_t datos;
-	estados_t estados = silencio;//ESTADO INICIAL//
-
-	int i=0;
-	char eleccion,elec;
-	char dias[1][7][20];
-	//CREANDO EL ARCHIVO DE CONFIGURACION//
-	FILE *alarmap;
-	if((alarmap=fopen("alarmap","wb"))==NULL){
-		printf("NO SE PUDO LEER EL ARCHIVO");
-	}
-	else{
-	printf("--------------------------------------------------ALARMA DESPERTADOR---------------------------------------------------\n");
-	printf("*AGREGAR ALARMA\n");
-	printf("\tHORA: ");
-	scanf("%i",&datos.hora);
-	printf("\tMINUTOS: ");
-	scanf("%i",&datos.minutos);
-	fflush(stdin);
-    do{
-		    printf("*LUNES/MARTES/MIERCOLES/JUEVES/VIERNES/SABADO/DOMINGO*\n");
-           	printf("SELECCIONE EL DIA: ");
-		    fflush(stdin);
-		    gets(dias[0][i]);
-		    fflush(stdin);
-		    i++;
-		    printf("AGREGAR OTRO DIA (S/N): ");
-		    scanf("%c",&eleccion);
-		    
-			
-		}while(eleccion=='S'|| eleccion=='s');
-	     fflush(stdin);	
-	     printf("REPETIR (S/N): ");
-         scanf("%c",&elec);
-	     if(elec=='S' || elec== 's'){
-	     	printf("\n\t5 MINUTOS\n\t10 MINUTOS\n\t15 MINUTOS\n\t30 MINUTOS\n");
-	    	scanf("%i",&datos.extension);
-	      }
-		   system("cls");
-	       fflush(stdin);
-	       fwrite(&datos,sizeof(datos),1,alarmap);
-		   fclose(alarmap);
-	  }//FIN DE LA CREACION DEL ARCHIVO DE CONFIGURACION//
-	  est_t est = activada;
-	  
-	if((alarmap=fopen("alarmap","rb"))==NULL){
-	   	printf("NO SE PUEDE LEER EL ARCHIVO DE CONFIGURACION");
-	   } 
-	   
-    else{
-	   	
-	   	fread(&datos,sizeof(datos),1,alarmap);
-	   	if(datos.minutos<10){
-	    	printf("ALARMA PROGRAMADA: %i:0%i HS",datos.hora,datos.minutos);
-	    	
-		 }
-		else{
-			printf("ALARMA PROGRAMADA: %i:%i HS",datos.hora,datos.minutos);
-		 }
-		
-		fflush(stdin);
-	    printf("\nDIA/S: ");
-	    int j;
-	    for(j=0;j<i;j++)
-	    {
-	    	printf("%s ",dias[0][j]);
-		}
-		printf("\n");
-		printf("*REPETIR A LOS %i MINUTOS\n",datos.extension);
-		
-		system("pause");
-	    system("cls");
-	   	fclose(alarmap);
-	   }
-	   
-	   while(1){
+	datos = inicio();
+	est_t est = activada;
+	estados_t estados = silenciada;//ESTADO INICIAL//
+	while(1){
 	   	switch(estados){
-	   		case silencio: estados = silencio_t(datos);
+	   		case silenciada: estados = silencio(datos);
 	   		   break;
 	   		    
-	   		case alarma: estados = alarma_t(datos);
+	   		case disparar: estados = alarma(datos);
 			   break;
 			   
-			case posponer: estados = posponer_t(datos);
+			case pospuesta: estados = posponer(datos);
 			   break;       
 		   }
 	   }
-	
-	
-	return 0;
 }
-
